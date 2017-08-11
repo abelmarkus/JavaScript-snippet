b206a80ecf9b4e148f602506c1c85c3e2cd40ef2
abelmarkus <abelmarkus@gmail.com>
2017-08-11 15:40:11 +0200

    Create remote-script.js

---
+++ remote-script.js
@@ -0,0 +1,6 @@
+javascript:(function(){
+	var script=document.createElement('script');
+  // replace script.src value with the URL of the desired script
+	script.src='https://rawgit.com/abelmarkus/VVVVID/master/vvvvid_skip_ads.js';
+	document.head.appendChild(script);
+})();
