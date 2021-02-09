dad7d8f9b742d54593e1b1ea4ce9949d0c527072
abelmarkus <abelmarkus@gmail.com>
2021-02-09 14:31:34 +0100

    Update Reveal-password.js

--- Reveal-password.js
+++ Reveal-password.js
@@ -1,3 +1,3 @@
-javascript: ((function() {
+javascript: (function() {
     for (var inputs = document.querySelectorAll("[type=password]"), i = 0; i < inputs.length; i++) inputs[i].setAttribute("type", "text");
-})())();
+})();
