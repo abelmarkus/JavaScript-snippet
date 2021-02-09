2a91322cfc672bf6302371c03c302a40a6aa2f1b
abelmarkus <abelmarkus@gmail.com>
2021-02-09 14:30:38 +0100

    Update Reveal-password.js

--- Reveal-password.js
+++ Reveal-password.js
@@ -1,4 +1,3 @@
 javascript: ((function() {
     for (var inputs = document.querySelectorAll("[type=password]"), i = 0; i < inputs.length; i++) inputs[i].setAttribute("type", "text");
-    document.body.addEventListener("click", myFunction), setTimeout(myFunction, 5e3)
 })())();
